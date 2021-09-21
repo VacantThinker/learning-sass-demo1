9fd0c40bfc13ebea86d76d5091c36045447d2e83
VacantThinker <50100840+VacantThinker@users.noreply.github.com>
2021-09-21 18:14:59 +0800

    'update'

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const critical = require("critical");
 
-critical.generate({
-    src: 'index.html',
-    inline: true,
-    base: 'test/',
-    target: 'index-critical.html',
-    width: 1300,
-    height: 900,
-});
+critical
+    .generate(
+        {
+            src: 'index.html',
+            css: 'index.css',
+            inline: true,
+            base: 'dist/',
+            width: 1300,
+            height: 900,
+            target: {
+                css: 'index.css',
+                html: 'index.html'
+            },
+        },
+        null)
+    .then(r => null);
