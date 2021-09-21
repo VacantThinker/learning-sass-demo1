d6fa622e33fda232c6b90899987c66fb0e1fb328
VacantThinker <50100840+VacantThinker@users.noreply.github.com>
2021-09-21 13:15:40 +0800

    'update'

---
+++ index.js
@@ -0,0 +1,10 @@
+const critical = require("critical");
+
+critical.generate({
+    inline: true,
+    base: 'test/',
+    src: 'index.html',
+    target: 'index-critical.html',
+    width: 1300,
+    height: 900,
+});
